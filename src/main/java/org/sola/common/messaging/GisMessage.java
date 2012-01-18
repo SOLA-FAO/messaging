5d4b3f1b417d5eb7bcb684b01b53bc32551eea63
Elton Manoku <elton.manoku@fao.org>
2012-01-18 17:54:38 +0100

    Added client side tools for cadastre redefinition. Added also server-side to retrieve nodes. Some database changes.
    Added tools: add node, modify/delete node.
    Added web service method to retrieve node and related parcels.
    Added web service method to retrieve potential node and related parcels.
    Added business rules and supporting functions to generate ba unit numbers.

--- src/main/java/org/sola/common/messaging/GisMessage.java
+++ src/main/java/org/sola/common/messaging/GisMessage.java
@@ -161,6 +161,15 @@ public class GisMessage {
     /**Select a node to remove*/
     public static final String CADASTRE_TOOLTIP_REMOVE_NODE  = CADASTRE_CHANGE + "021";
 
+    /**Coordinates entered are not valid.*/
+    public static final String CADASTRE_REDEFINITION_COORDS_NOT_VALID  = CADASTRE_CHANGE + "022";
+
+    /**Node is not found. This is a serious error. */
+    public static final String CADASTRE_REDEFINITION_NODE_NOTFOUND  = CADASTRE_CHANGE + "023";
+    
+    /**Node is related to more than 2 parcels.*/
+    public static final String CADASTRE_REDEFINITION_NODE_HAS_MORE_THAN_ONE_CO  = CADASTRE_CHANGE + "024";
+    
     // Messages related with the left panel
     /* Layers */
     public static final String LEFT_PANEL_TAB_LAYERS_TITLE = LEFT_PANEL + "01";
