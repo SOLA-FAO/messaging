ff811978eaca5d13cc21696c6700acc5db1abbb3
Andrew McDowell <armcdowell@hotmail.com>
2012-07-16 20:37:56 +1200

    Added file size restriction to file upload [#155 state:fixed responsible:none]
    File upload will report an error if the file is > 20Mb (client) or
    > 30Mb (Server) when trying to upload a file. Also updated the
    memory settings for the web start to support display of files up to
    30Mb. Further work required to load files using streaming, but have
    raised another ticket to cover that.

--- src/main/java/org/sola/common/messaging/ServiceMessage.java
+++ src/main/java/org/sola/common/messaging/ServiceMessage.java
@@ -95,6 +95,8 @@ public class ServiceMessage {
     public static final String EXCEPTION_INITALIZE_WSCLIENT = EXCEPTION + "008";
     /* serexcp009 - The username and password are incorrect. */
     public static final String EXCEPTION_INVALID_SECURITY_HEADER = EXCEPTION + "009";
+    /* serexcp010 - The file cannot be attached due to its size ({0}MB) */
+    public static final String EXCEPTION_FILE_TOO_BIG = EXCEPTION + "010";
 
     
     // Business Rule Messages
